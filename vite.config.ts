94bd2361461d12873914cbd6a7fccd1a96812a22
jayeshgpatel2 <168744707+jayeshgpatel2@users.noreply.github.com>
2025-12-07 20:47:28 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: '/fba-stock-viewer-pro/',  // Change this to your repository name
+      base: '/',  // Change this to your repository name
       server: {
         port: 3000,
         host: '0.0.0.0',
